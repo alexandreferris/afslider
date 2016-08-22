0fcb5bac975084a621a6751fc8329eab1e9ef056
Alexandre Ferris <ferris.alexandre@gmail.com>
2016-08-22 16:52:16 -0300

    new random logo, added autoplay, fadeIn/fadeOut, speed/time, rearranged css stylesheet

--- public_html/index.php
+++ public_html/index.php
@@ -4,41 +4,14 @@
 	<meta charset="UTF-8">
 	<title>Slide Plugin</title>
 	
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+
 	<!-- JS / jQuery -->
 	<script type="text/javascript" src="/js/jquery-2.2.4.js"></script>
 	<script type="text/javascript" src="/js/slide.js"></script>
 </head>
 <body>
-	<style>
-		.logo {
-			width: 150px;
-    		margin-left: 20px;
-    		margin-top: 20px;
-		}
-		#slide {
-			background-color: #009900;
-		}
-		#slide .item img {
-			width: 100vw;
-			height: 100vh;
-			position: absolute;
-			top: 0px;
-			left: 0px;
-		}
-		#slide .item span {
-			width: 80vw;
-			max-width: 80vw;
-			margin-left: 10vw;
-			margin-right: 10vw;
-			position: absolute;
-			z-index: 1;
-			font-size: 40px;
-			text-align: center;
-			white-space:normal;
-			color: white;
-		}
-	</style>
-
 	<div id="slide">
 		<div class="item">
 			<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan lectus a congue facilisis.</span>
@@ -50,12 +23,16 @@
 		</div>
 	</div>
 
-	<!-- <h1>Slide Plugin</h1> -->
-	<img src="img/logo.png" class="logo" />
+	<div style="width: 100vw;height:100vh;float:left;text-align:center;">
+		<img src="img/logo.png" class="logo" />
+	</div>
+
+	<h1>Slide Plugin</h1>
+	
 
 	<script type="text/javascript">
 		$("#slide").afslider({
-			texto: 'Meu outro texto'
+			interval: '3000'
 		});
 	</script>
 </body>
--- public_html/js/slide.js
+++ public_html/js/slide.js
@@ -1,12 +1,15 @@
 /*************************************
 Author: Alexandre Ferris
-Year: 2016
-Website: http://rivott.com.br
 E-mail: contato@rivott.com.br
+Website: http://rivott.com.br
+Built in: 2016
 *************************************/
 (function ( $ ) {
 	$.fn.afslider = function( opts ) {
 		var options = $.extend( {}, $.fn.afslider.defaults, opts);
+		var currentIndex = 0;
+		var slideItems = $(this).children('div.item');
+		var slideAmount = slideItems.length;
 
 		/* Slide outer dimensions */		
 		this.css({
@@ -33,18 +36,32 @@ E-mail: contato@rivott.com.br
 
 		/* Hidding Slides */
 		$(this).children('div.item').not(":first-child").hide();
+
+		/* if autoplay enabled start moving slides */
+		if(options.autoplay) {
+			/* setInterval for loop */
+			var sliderAutoPlay = setInterval(function() {
+				currentIndex += 1;
+				if(currentIndex > (slideAmount - 1)) {
+					currentIndex = 0;
+				}
+
+				/* get next slide */
+				var nextSlide = slideItems.eq(currentIndex);
+				slideItems.fadeOut(2000);
+				nextSlide.fadeIn(2000);
+			}, options.interval);
+		}
 	};
 	
 	$.fn.afslider.defaults = {
-		texto: 'Meu texto',
-		
-
 		/* Default variables */
 		responsive: true,		/* True or False */
 		position: 'absolute',		/* static, relative, fixed, absolute */
 		width: '100vw',			/* 100vw or set width */
 		height: '100vh',		/* 100vh or set height */
 		zindex: '-1',
-		autoplay: true			/* True of False */
+		autoplay: true,			/* True of False */
+		interval: '1000'			/* Interval between each slide */
 	}
 }( jQuery ));
